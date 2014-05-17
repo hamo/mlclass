74784335d4f81c14e7fb6d16458fa5a62bf7433f
Yang Bai <hamo.by@gmail.com>
2014-05-17 12:39:34 +0800

    Anomaly Detection and Recommender Systems

--- mlclass-ex8-005/mlclass-ex8/cofiCostFunc.m
+++ mlclass-ex8-005/mlclass-ex8/cofiCostFunc.m
@@ -40,9 +40,15 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+%J = sum(sum(((X * Theta' - Y) .* R) .^ 2)) / 2;
+J = sum(sum(((X * Theta' - Y) .* R) .^ 2)) / 2 + sum(sum(Theta .^ 2)) * lambda / 2 + sum(sum(X .^ 2)) * lambda / 2;
 
 
+%X_grad = ((X * Theta' - Y) .* R) * Theta;
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda * X;
 
+%Theta_grad = ((X * Theta' - Y) .* R)' * X;
+Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda * Theta;
 
 
 
